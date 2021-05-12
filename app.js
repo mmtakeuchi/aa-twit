2b83b6a1b0391ff82a520e3156404d3d3e8b7ee0
mmtakeuchi <mikemtaki@gmail.com>
2021-05-12 15:14:37 -0700

    create user and tweets routes

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+const port = process.env.PORT || 5000;
 const express = require("express");
+const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
-const db = require("./config/keys").mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get("/", (req, res) => res.send("Nodemon is working"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());

fee421c54811d94bf62d66123a186e62ca984eb1
mmtakeuchi <mikemtaki@gmail.com>
2021-05-12 19:32:38 -0700

    implement user register route

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const port = process.env.PORT || 5000;
 const express = require("express");
-const db = require("./config/keys").mongoURI;
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("Nodemon is working"));
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.listen(port, () => console.log(`Server is running on port ${port}`));

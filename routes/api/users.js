6d49ce143ac9e603b3b9f145cb86320a73a427b4
mmtakeuchi <mikemtaki@gmail.com>
2021-05-13 09:47:58 -0700

    create valid text function to check valid strings

--- routes/api/users.js
+++ routes/api/users.js
@@ -77,7 +77,7 @@ router.post("/login", (req, res) => {
 
     bcrypt.compare(password, user.password).then((isMatch) => {
       if (isMatch) {
-        const payload = { id: user.id, handle: user.handle };
+        const payload = { id: user.id, handle: user.handle, email: user.email };
 
         jwt.sign(
           payload,

e7430d112680c41884c417287d37e93db8e451b8
mmtakeuchi <mikemtaki@gmail.com>
2021-05-13 13:42:50 -0700

    create session reducer for logout action

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,3 +1,6 @@
+import * as APIUtil from "../util/session_api_util";
+import jwt_decode from "jwt-decode";
+
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 
 export const logoutUser = () => ({

cc34cdfc8cf2b529cfb7dd4b10c9fb3b03bc8bb3
mmtakeuchi <mikemtaki@gmail.com>
2021-05-13 14:54:55 -0700

    create tweet reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer";
+import tweet from "./tweet_reducer";
 import errors from "./errors_reducer";
 
 const RootReducer = combineReducers({
   session,
+  tweet,
   errors,
 });
 
